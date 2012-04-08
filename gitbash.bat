e7485c7eac8197efbaefd9586bfcc10f6459b615
C.c <fishbone1319@gmail.com>
2012-04-08 23:14:13 +0400

    gitbash

--- gitbash.bat
+++ gitbash.bat
@@ -2,7 +2,7 @@
 
 rem THIS IS KOSTYL
 if exist "C:\Program Files\Git\bin\sh.exe" (
-	cmd /c C:\Program Files\Git\bin\sh.exe --login -i
+	cmd /c "C:\Program Files\Git\bin\sh.exe" --login -i
 ) else (
 	if exist "d:\Program Files (x86)\Git\bin\sh.exe" (
 		rem For KIRUSHA's computer
